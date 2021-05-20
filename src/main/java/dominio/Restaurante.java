a262f54151401b4927bcdfeffa95e1e1cd1bc95f
JoseFcoExamen <josefcoexamen@gmail.com>
2021-05-20 19:43:51 +0200

    Modificada clase Restaurante

--- src/main/java/dominio/Restaurante.java
+++ src/main/java/dominio/Restaurante.java
@@ -7,4 +7,55 @@ public class Restaurante {
     private String tipo;
     private String direccion;
     private int horario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getHorario() {
+        return horario;
+    }
+
+    public void setHorario(int horario) {
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurante{" +
+                "nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", horario=" + horario +
+                '}';
+    }
 }
